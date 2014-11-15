e9a5cc5e8a0c998bb498d52e28e432ae7b511bbb
marcwright <marc.wright@gmail.com>
2014-11-15 09:37:39 -0500

    first angular-rails commit added angular and bootstrap to Bowerfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Receta2::Application.config.secret_key_base = '907f3c2a4fffd8c5d1ed5b1846f04dd94bf5ab7faab422950eacb49d412d2adab7b1907720300fe50061c0684096a792b4ce49ca75c24581cf38886a007b3620'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Receta2::Application.config.session_store :cookie_store, key: '_receta2_session'
